1833443717246a3c8c9717337b6aec458ed2a9a2
AnkurPersonalGitRepository <8751714+ANKGITDEMOS@users.noreply.github.com>
2020-05-25 19:02:33 +0530

    Added script - You may use it as runbook

---
+++ DeallocateStoppedVM.ps1
@@ -0,0 +1,29 @@
+﻿Connect-AzureRMAccount
+
+
+Set-AzureRMContext -subscriptionid XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+$RGs = Get-AzureRMResourceGroup
+
+foreach  ($RG in $RGs)
+  {
+    $VMs = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName
+    Foreach  ($VM in $VMs)
+    {
+      $VMDetail = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Status
+      $RGN = $VMDetail.ResourceGroupName  
+      foreach ($VMStatus in $VMDetail.Statuses)
+      { 
+          if($VMStatus.DisplayStatus -eq "VM stopped"){
+            Write-Output $VMStatus
+            $VMStatusDetail = $VMStatus.DisplayStatus
+            Write-Output "Found Stopped VM Resource Group: $RGN", ("VM Name: " + $VM.Name), "Status: $VMStatusDetail"
+            Stop-AzureRmVM -ResourceGroupName $VMDetail.ResourceGroupName -Name $VM.Name
+            Write-Output "Initiated deallocation of VM Resource Group: $RGN", ("VM Name: " + $VM.Name), "Status: $VMStatusDetail"
+
+          }
+      }
+    }
+  }
+
+
